9787a7264df2db678f5b24b3b2640194d104c797
NafisMahmudAyon <nafismahmudayon@gmail.com>
2023-04-11 15:53:37 +0600

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
     const fetchBotResponse = async () => {
         const { data } = await axios.post(
-            "https://tubular-axolotl-c24839.netlify.app/",
+            "https://chatbot-d6cg.onrender.com",
             // "http://localhost:5173",
             { input },
             {
